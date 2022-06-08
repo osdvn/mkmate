2cc73a1c41db30015ff9fdcd44bf5f58eca6a99c
Dzung Do <dungdo@osd.vn>
2022-06-09 04:48:02 +0700

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports  =  {
+  title: 'Mkmate Docs',
+  description: 'Manual Guide',
+  dest: 'docs',
+  themeConfig: {
+      sidebar: 'auto',
+      nav: [
+        { text: 'Trang Chủ', link: '/' },
+        { text: 'Giới Thiệu', link: '/about/' }
+      ],
+    }
+}
